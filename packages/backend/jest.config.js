34dbd824aace66913ff8aa92f65cbbf74ddbf703
Alcir Junior <junior@cajucomunica.com.br>
2021-09-21 10:34:40 -0300

    chore: update jest config

--- packages/backend/jest.config.js
+++ packages/backend/jest.config.js
@@ -3,4 +3,9 @@ const { name } = require('./package.json');
 module.exports = {
   displayName: name,
   name,
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/*.ts'],
+  coverageDirectory: './coverage',
+  coverageReporters: ['text-summary', 'text', 'lcov'],
+  verbose: true,
 };
